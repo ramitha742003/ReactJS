2fa603c821c9c3cab16a70df52d9e82f18740363
Ramitha R <167272050+ramitha742003@users.noreply.github.com>
2025-05-27 18:00:36 +0530

    Delete foodapp/foodapp directory

--- foodapp/foodapp/src/components/InnerContainer.jsx
+++
@@ -1,4 +0,0 @@
-import styles from "./innercontainer.module.css";
-export default function InnerContainer({ children }) {
-  return <div className={styles.innerContainer}>{children}</div>;
-}
--- foodapp/foodapp/src/components/Nav.jsx
+++
@@ -1,4 +0,0 @@
-import styles from "./nav.module.css";
-export default function Nav() {
-  return <div className={styles.nav}>FoodApp</div>;
-}
